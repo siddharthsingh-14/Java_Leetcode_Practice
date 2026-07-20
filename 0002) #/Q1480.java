b6209d4d034a6b5ae625efbf399130d4aaff8ccf
my-java-journey <sidd03072006@gmail.com>
2026-07-21 00:55:03 +0530

    solved question 14800

---
+++ 0002) #/Q1480.java
@@ -0,0 +1,12 @@
+class Solution1480 {
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] ans= new int[nums.length];
+        for(int i = 0; i<nums.length;i++){
+            sum +=nums[i];
+            ans[i]=sum;           
+        }
+        return  ans;
+
+    }
+}
