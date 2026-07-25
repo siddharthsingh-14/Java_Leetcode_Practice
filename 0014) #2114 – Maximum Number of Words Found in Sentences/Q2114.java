cdae08cec2246dc2e833485456d58f76531172d3
my-java-journey <sidd03072006@gmail.com>
2026-07-25 17:13:52 +0530

    solved question 2114

---
+++ 0014) #2114 – Maximum Number of Words Found in Sentences/Q2114.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int count = 1;
+        int max= count;
+        for(int i = 0; i<sentences.length;i++){
+            count = 1;
+            for(int j= 0; j<sentences[i].length();j++){
+                if(sentences[i].charAt(j)==' '){
+                    count++;
+                }
+            }
+            if(count>max){
+                max=count;
+            }
+
+        }
+        return  max;
+    }
+}
