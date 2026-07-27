2733a12d57e7a93e01de4eebdca237663a1b764c
my-java-journey <sidd03072006@gmail.com>
2026-07-27 21:51:29 +0530

    solved question 1678

---
+++ 0018) #1678 – Goal Parser Interpretation/Q1678.java
@@ -0,0 +1,19 @@
+class Solution1678 {
+    public String interpret(String command) {
+        StringBuilder ans =  new StringBuilder();
+        for(int i = 0 ;  i < command.length(); i++){
+            if(command.charAt(i)=='G'){
+                ans.append('G');
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                ans.append("o");
+                i++;
+            }
+            else if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                ans.append("al");
+                i+=3;
+            }
+        }
+        return ans.toString();
+    }
+}
