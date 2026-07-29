432fd95be3a88d5a4f3ebeb9c268ac1b2b9a9b5e
my-java-journey <sidd03072006@gmail.com>
2026-07-29 11:32:14 +0530

    solved question 1221

---
+++ Q1221.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int  count=0;
+        int Lcount=0;
+        int Rcount=0;
+        for(int i = 0 ; i<s.length();i++){
+            if(s.charAt(i)=='L'){
+                Lcount++;
+            }
+            else if(s.charAt(i)=='R'){
+                Rcount++;
+            }
+            if(Rcount == Lcount){
+                count++;
+            }
+        }
+        return count;
+    }
+}
