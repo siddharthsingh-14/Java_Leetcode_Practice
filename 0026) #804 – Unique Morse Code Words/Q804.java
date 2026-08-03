ce281a8456f06beb1e5bcd4e094c1a22cfed1ada
my-java-journey <sidd03072006@gmail.com>
2026-08-03 22:33:30 +0530

    solved question 804

---
+++ 0026) #804 – Unique Morse Code Words/Q804.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {
+        ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+        "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+        "..-","...-",".--","-..-","-.--","--.."
+        };
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0; i<words.length;i++){
+            StringBuilder sb = new StringBuilder();
+            String word = words[i];
+            for(int j = 0; j<word.length();j++){
+                char ch = word.charAt(j);
+                sb.append(morse[ch - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+     
+    }
+}
