eef9bb8db9a4050619b4a0fe5242e8ebdf92dbf6
my-java-journey <sidd03072006@gmail.com>
2026-08-11 00:12:00 +0530

    Solved Question 217

---
+++ 0031) #217 – Contains Duplicate/Q217.java
@@ -0,0 +1,9 @@
+class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        return(set.size()!=nums.length);
+    }
+}
