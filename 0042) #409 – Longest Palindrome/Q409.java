169697e389a8ed70747be63d74d29562c8c3906e
my-java-journey <sidd03072006@gmail.com>
2026-08-27 22:20:56 +0530

    quesiton number 409

---
+++ 0042) #409 – Longest Palindrome/Q409.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution409 {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int  count  = 0;
+        boolean  isoddpresent = false;
+        for(int i =0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int  value : map.values()){
+            if(value % 2 == 0){
+                count += value;
+            }
+            else if(value % 2 == 1){
+                count += value-1;
+                isoddpresent = true;
+            }
+            
+        }
+        if(isoddpresent){
+            count++;
+        }
+        
+        return count ;
+    }
+}
