011b8ddb793a1a118ea969ab68cc72682d9e4cda
my-java-journey <sidd03072006@gmail.com>
2026-09-18 11:19:43 +0530

    quesiton number 88

---
+++ 0057) # 88. Merge Sorted Array/Q88.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int  j = 0;
+        for(int i =m ; i<m+n;i++){
+            if(nums1[i]==0){
+                nums1[i]=nums2[j];
+                if(j<n-1) j++;
+                
+            }
+        }
+        Arrays.sort(nums1);
+    }
+}
