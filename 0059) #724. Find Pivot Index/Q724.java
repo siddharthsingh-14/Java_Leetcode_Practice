3cde68526cc6ca9f554b514a331358d822089a52
my-java-journey <sidd03072006@gmail.com>
2026-09-20 19:25:33 +0530

    quesiton number 724

---
+++ 0059) #724. Find Pivot Index/Q724.java
@@ -0,0 +1,23 @@
+class Solution724 {
+    public int pivotIndex(int[] nums) {
+
+        int rightSum = 0, leftSum  = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+
+            leftSum = 0;
+            for(int j = 0; j < i; j++){
+                leftSum += nums[j];
+            }
+
+            rightSum = 0;
+            for(int j = i + 1; j < nums.length; j++){
+                rightSum += nums[j];
+            }
+
+            if(leftSum == rightSum) return i;
+        }
+
+        return -1;
+    }
+}
